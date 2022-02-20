740d630393d88188ab28b124f531e052114c8c88
blobcode <lhelme123@gmail.com>
2022-02-19 23:34:33 -0500

    implement demo functionality

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,30 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+use ahash::RandomState;
+use std::error::Error;
+use std::fs;
+use std::{collections::HashMap, path::PathBuf};
+
+pub struct Data {
+    map: HashMap<String, String, RandomState>,
+}
+
+impl Data {
+    /// read database file
+    pub fn open<P: Into<PathBuf> + std::convert::AsRef<std::path::Path>>(file: P) -> Data {
+        let result = Data {
+            map: HashMap::default(),
+        };
+        result
+    }
+
+    /// insert value into database
+    pub fn insert<S: Into<String>>(&mut self, key: S, value: S) {
+        self.map.insert(key.into(), value.into());
+    }
+
+    /// retrive value from database
+    pub fn get<S: Into<String>>(&self, key: S) -> String {
+        // read from passed hashmap
+        let result = self.map.get(&key.into()).unwrap().to_string();
+        result
     }
 }
