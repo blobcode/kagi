62d684d6df5e610e589648ab8815272a7c0739bf
blobcode <lhelme123@gmail.com>
2022-02-20 22:06:51 -0500

    add file handling

--- src/store.rs
+++ src/store.rs
@@ -35,29 +35,62 @@ impl Store {
         let result = self.map.get(&key.into()).unwrap().to_string();
         result
     }
+
+    pub fn create<P: Into<PathBuf> + std::convert::AsRef<std::path::Path>>(file: P) -> Self {
+        // load path
+        let path = file.into();
+
+        Self {
+            map: HashMap::default(),
+            file: path,
+        }
+    }
+
+    pub fn default() -> Self {
+        Self {
+            map: HashMap::default(),
+            file: PathBuf::new(),
+        }
+    }
 }
 
 /// read database file
 pub fn open<P: Into<PathBuf> + std::convert::AsRef<std::path::Path>>(file: P) -> Store {
     // load path
     let path = file.into();
-    let file = path.clone();
+
+    let mut result: Store = Store::default();
+
+    // default config for bincode
+    let config = config::standard();
 
     // read from file
     let mut encoded = Vec::new();
-    let mut f = File::open(path).unwrap();
-    f.read_to_end(&mut encoded).unwrap();
-
-    // default config
-    let config = config::standard();
 
     // initialize struct
-    let data: (Store, usize) = bincode::decode_from_slice(&encoded[..], config).unwrap();
 
-    let result = Store {
-        map: data.0.map,
-        file,
-    };
+    if path.exists() {
+        let mut f = File::open(&path).unwrap();
+        f.read_to_end(&mut encoded).unwrap();
+
+        let data: (Store, usize) = bincode::decode_from_slice(&encoded[..], config).unwrap();
+
+        result = Store {
+            map: data.0.map,
+            file: path,
+        };
+    } else {
+        let f = File::create(&path).unwrap();
+        let mut f = BufWriter::new(f);
+
+        result = Store {
+            map: HashMap::default(),
+            file: path,
+        };
+
+        let encoded: Vec<u8> = bincode::encode_to_vec(&result, config).unwrap();
+        f.write_all(&encoded).unwrap();
+    }
 
     result
 }
